5c2f71eca2916e3aa8c6cba64b5282ff8526a9c8
Manu Kapolke <manukapolke@gmail.com>
2023-09-07 16:19:16 +0200

    added firebase with observable todos$ variable

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'mk-todolist',
+    appId: '1:207042172435:web:6da424795c863176d851e6',
+    storageBucket: 'mk-todolist.appspot.com',
+    apiKey: 'AIzaSyCOgNrysByqPaDHA9qzhM1igpKXAUuzsbA',
+    authDomain: 'mk-todolist.firebaseapp.com',
+    messagingSenderId: '207042172435',
+  },
+};
